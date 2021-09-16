0fec4e0bf3b49c84a92187f0d2bed4fb9faaf3d1
EskalarteDexter <88998149+EskalarteDexter@users.noreply.github.com>
2021-09-16 10:53:39 +0800

    Update slowdns.sh

--- slowdns.sh
+++ slowdns.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#slowdns (Wegare)
+#slowdns (Dexter Eskalarte)
 stop () {
 host="$(cat /root/akun/slowdns.txt | tr '\n' ' '  | awk '{print $1}')" 
 route="$(cat /root/akun/ipmodem.txt | grep -i ipmodem | cut -d= -f2 | tail -n1)" 
@@ -16,7 +16,17 @@ ns2="$(cat /root/akun/slowdns.txt | tr '\n' ' '  | awk '{print $4}')"
 udp2="$(cat /root/akun/slowdns.txt | tr '\n' ' '  | awk '{print $5}')"
 pubkey2="$(cat /root/akun/server.pub)" 
 clear
-echo "Inject slowdns by wegare"
+echo '
+ /$$$$$$$                       /$$                        
+| $$__  $$                     | $$                        
+| $$  \ $$  /$$$$$$  /$$   /$$/$$$$$$    /$$$$$$   /$$$$$$ 
+| $$  | $$ /$$__  $$|  $$ /$$/_  $$_/   /$$__  $$ /$$__  $$
+| $$  | $$| $$$$$$$$ \  $$$$/  | $$    | $$$$$$$$| $$  \__/
+| $$  | $$| $$_____/  >$$  $$  | $$ /$$| $$_____/| $$      
+| $$$$$$$/|  $$$$$$$ /$$/\  $$ |  $$$$/|  $$$$$$$| $$      
+|_______/  \_______/|__/  \__/  \___/   \_______/|__/ 
+'
+echo "Inject slowdns by: Dexter Eskalarte"
 echo "1. Sett Profile"
 echo "2. Start Inject"
 echo "3. Stop Inject"
@@ -27,27 +37,27 @@ read -p "(default tools: 2) : " tools
 [ -z "${tools}" ] && tools="2"
 if [ "$tools" = "1" ]; then
 
-echo "Masukkan ip/host" 
+echo "Enter ip/host" 
 read -p "default ip/host: $host2 : " host
 [ -z "${host}" ] && host="$host2"
 
-echo "Masukkan username" 
+echo "Enter username" 
 read -p "default username: $user2 : " user
 [ -z "${user}" ] && user="$user2"
 
-echo "Masukkan password" 
+echo "Enter password" 
 read -p "default password: $pass2 : " pass
 [ -z "${pass}" ] && pass="$pass2"
 
-echo "Masukkan NS" 
+echo "Enter NS" 
 read -p "default NS: $ns2 : " ns
 [ -z "${ns}" ] && ns="$ns2"
 
-echo "Masukkan pubkey" 
+echo "Enter pubkey" 
 read -p "default pubkey: $pubkey2 : " pubkey
 [ -z "${pubkey}" ] && pubkey="$pubkey2"
 
-echo "Masukkan port udpgw" 
+echo "Enter udpgw port" 
 read -p "default udpgw: $udp2 : " udp
 [ -z "${udp}" ] && udp="$udp2"
 badvpn="badvpn-tun2socks --tundev tun1 --netif-ipaddr 10.0.0.2 --netif-netmask 255.255.255.0 --socks-server-addr 127.0.0.1:1080 --udpgw-remote-server-addr 127.0.0.1:$udp --udpgw-connection-buffer-size 65535 --udpgw-transparent-dns &"
@@ -62,14 +72,14 @@ cat <<EOF> /usr/bin/gproxy-slowdns
 $badvpn
 EOF
 chmod +x /usr/bin/gproxy-slowdns
-echo "Sett Profile Sukses"
+echo "Set Profile Success"
 sleep 2
 clear
 /usr/bin/slowdns
 elif [ "${tools}" = "2" ]; then
 cek="$(cat /root/akun/slowdns.txt)"
 if [[ -z $cek ]]; then
-echo "anda belum membuat profile"
+echo "You haven't created a profile yet"
 exit
 fi
 stop
@@ -94,34 +104,34 @@ route add "$host" gw "$route" metric 0
 route add default gw 10.0.0.2 metric 0
     
 echo "#!/bin/bash
-#stl (Wegare)
+#sts (Dexter Eskalarte)
 host=$(cat /root/akun/slowdns.txt | tr '\n' ' '  | awk '{print $1}')
 fping -l $host" > /usr/bin/ping-slowdns
 chmod +x /usr/bin/ping-slowdns
 /usr/bin/ping-slowdns > /dev/null 2>&1 &
 elif [ "${tools}" = "3" ]; then
 stop
-echo "Stop Suksess"
+echo "Stop Success"
 sleep 2
 clear
 /usr/bin/slowdns
 elif [ "${tools}" = "4" ]; then
 cat <<EOF>> /etc/crontabs/root
 
-# BEGIN AUTOREKONEKSLOWDNS
+# BEGIN AUTORECONNECT SLOWDNS
 */1 * * * *  autorekonek-slowdns
-# END AUTOREKONEKSLOWDNS
+# END AUTORECONNECT SLOWDNS
 EOF
 sed -i '/^$/d' /etc/crontabs/root 2>/dev/null
 /etc/init.d/cron restart
-echo "Enable Suksess"
+echo "Enable Success"
 sleep 2
 clear
 /usr/bin/slowdns
 elif [ "${tools}" = "5" ]; then
-sed -i "/^# BEGIN AUTOREKONEKSLOWDNS/,/^# END AUTOREKONEKSLOWDNS/d" /etc/crontabs/root > /dev/null
+sed -i "/^# BEGIN AUTORECONNECT SLOWDNS# END AUTORECONNECT SLOWDNS/d" /etc/crontabs/root > /dev/null
 /etc/init.d/cron restart
-echo "Disable Suksess"
+echo "Disable Success"
 sleep 2
 clear
 /usr/bin/slowdns
