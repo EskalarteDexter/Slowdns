b75f5f85db19f25e1e54e35f548e3caa1eceb60b
EskalarteDexter <88998149+EskalarteDexter@users.noreply.github.com>
2021-09-16 10:55:42 +0800

    Update autorekonek-slowdns.sh

--- autorekonek-slowdns.sh
+++ autorekonek-slowdns.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#slowdns (Wegare)
+#slowdns (Dexter Eskalarte)
 route="$(route | grep -i 1.1.1.1 | head -n1 | awk '{print $2}')" 
 route2="$(route | grep -i 10.0.0.2 | head -n1 | awk '{print $2}')" 
 route3="$(lsof -i | grep -i ssh | grep -i 1080 | grep -i listen)" 
