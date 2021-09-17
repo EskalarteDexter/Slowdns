44e04e99e198117a6c8e676a82d6232c47ded824
EskalarteDexter <88998149+EskalarteDexter@users.noreply.github.com>
2021-09-17 22:18:03 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,17 +1,31 @@
 #!/bin/bash
 #slowdns (Dexter Eskalarte)
+slow()
+{
+echo "installing slowdns..."
 wget --no-check-certificate "https://raw.githubusercontent.com/EskalarteDexter/Slowdns/main/slowdns.sh/" -O /usr/bin/slowdns
+}
+slow1()
+{
+echo "autorekonek-slowdns..."
 wget --no-check-certificate "https://raw.githubusercontent.com/EskalarteDexter/Slowdns/main/autorekonek-slowdns.sh" -O /usr/bin/autorekonek-slowdns
+}
+slow2
+{
+echo "installing dna-client..."
 wget --no-check-certificate "https://raw.githubusercontent.com/EskalarteDexter/Slowdns/main/dns-client-v19" -O /usr/bin/dns-client
+}
 chmod +x /usr/bin/slowdns
 chmod +x /usr/bin/dns-client
 chmod +x /usr/bin/autorekonek-slowdns
-opkg install openvpn-openssl && opkg install ip-full && opkg install openssh-client && opkg install --force-depends *.ipk && opkg install lsof && opkg install fping screen python python3 coreutils-nohup
-rm -r ~/*.ipk
 rm -r ~/install.sh
 mkdir -p ~/akun/
 touch ~/akun/slowdns.txt
 touch ~/akun/ipmodem.txt 
+slow
+slow1
+slow2
 sleep 2
 echo "install complete"
 echo "to start the tool please run the command 'slowdns'"
+				
